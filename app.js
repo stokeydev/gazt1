58cf8c29b96fd36abbda64ee99590b0ccef34da8
stokeydev <120173316+stokeydev@users.noreply.github.com>
2024-08-13 01:36:18 +0100

    1st

---
+++ app.js
@@ -0,0 +1,49 @@
+// ---------------------------------------------------------
+// GLOBAL DECLARATIONS
+// ---------------------------------------------------------
+
+var map;
+
+// tile layers
+
+var streets = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"
+  }
+);
+
+var satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+  }
+);
+
+var basemaps = {
+  "Streets": streets,
+  "Satellite": satellite
+};
+
+// buttons
+
+var infoBtn = L.easyButton("fa-info fa-xl", function (btn, map) {
+  $("#exampleModal").modal("show");
+});
+
+// ---------------------------------------------------------
+// EVENT HANDLERS
+// ---------------------------------------------------------
+
+// initialise and add controls once DOM is ready
+
+$(document).ready(function () {
+  
+  map = L.map("map", {
+    layers: [streets]
+  }).setView([54.5, -4], 6);
+  
+  // setView is not required in your application as you will be
+  // deploying map.fitBounds() on the country border polygon
+
+  layerControl = L.control.layers(basemaps).addTo(map);
+
+  infoBtn.addTo(map);
+
+})
